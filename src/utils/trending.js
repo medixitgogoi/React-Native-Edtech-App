f3e366e6a8c383ac7e0efc9c19be73214ce420ea
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-11-16 17:16:39 +0530

    updated

---
+++ src/utils/trending.js
@@ -0,0 +1,37 @@
+export const trending = [
+    {
+        id: 1,
+        name: 'Maths for class VII',
+        teacher: 'Smith J.',
+        lessons: 19,
+        duration: 40,
+    },
+    {
+        id: 2,
+        name: 'Science for class IX',
+        teacher: 'Emily R.',
+        lessons: 15,
+        duration: 35,
+    },
+    {
+        id: 3,
+        name: 'Chemistry for class X',
+        teacher: 'John D.',
+        lessons: 12,
+        duration: 30,
+    },
+    {
+        id: 4,
+        name: 'Physics for class X',
+        teacher: 'Ava K.',
+        lessons: 20,
+        duration: 50,
+    },
+    {
+        id: 5,
+        name: 'Maths for class VIII',
+        teacher: 'Michael T.',
+        lessons: 14,
+        duration: 25,
+    },
+];
