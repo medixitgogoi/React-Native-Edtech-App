66031d6fd01ce747d40f9878bee163e16e953f95
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-11-16 17:26:26 +0530

    updated

---
+++ src/screens/Courses.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Courses = () => {
+    return (
+        <View>
+            <Text style={{ color: '#000' }}>All Courses</Text>
+        </View>
+    )
+}
+
+export default Courses;
