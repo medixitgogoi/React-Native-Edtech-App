0202f3e429ffb121704f4b6c220cca822596ff52
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-11-25 12:59:21 +0530

    updated

---
+++ src/screens/MyCourses.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyCourses = () => {
+    return (
+        <View>
+            <Text style={{ color: '#000' }}>MyCourses</Text>
+        </View>
+    )
+}
+
+export default MyCourses
