fde0d35cb1b1136822797bdb35884de104acda29
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-11-30 11:55:03 +0530

    updated

---
+++ src/screens/PrivacyPolicy.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const PrivacyPolicy = () => {
+    return (
+        <View>
+            <Text style={{ color: '#000' }}>PrivacyPolicy</Text>
+        </View>
+    )
+}
+
+export default PrivacyPolicy
