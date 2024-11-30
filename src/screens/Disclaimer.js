f4044cb34ba6d9bde54f9022a07d2766727219e8
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-11-30 11:54:42 +0530

    updated

---
+++ src/screens/Disclaimer.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Disclaimer = () => {
+    return (
+        <View>
+            <Text style={{ color: '#000' }}>Disclaimer</Text>
+        </View>
+    )
+}
+
+export default Disclaimer;
