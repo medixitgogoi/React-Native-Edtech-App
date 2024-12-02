df113af15cfe2caf3069f2bbd84b685a31010397
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-12-02 13:25:12 +0530

    updated

--- src/utils/fetchBoards.js
+++ src/utils/fetchBoards.js
@@ -4,9 +4,9 @@ import { Alert } from "react-native";
 export const fetchBoards = async () => {
     try {
         const response = await axios.get('/board/');
-        console.log('response: ', response);
+        // console.log('response: ', response);
 
-        return response?.data?.grocery; // Return data inside the try block after receiving the response
+        return response?.data?.data?.boards; // Return data inside the try block after receiving the response
     } catch (error) {
         Alert.alert("Error", error.message); // Add a title to the alert
         return null; // Return null in case of error
